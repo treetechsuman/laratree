d42c4d607e56a60a541ab8557819b83158f89b92
Suman Dahal <me.suman11@gmail.com>
2017-01-21 15:54:03 +0545

    login and register error is fixed

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -68,12 +68,14 @@
         </form>
         <div class="social-auth-links text-center">
           <p>- OR -</p>
-          <a href="{{url('organization/signup')}}" class="btn btn-block btn-social btn-facebook btn-flat"></i> SignUp New Organization</a>
-          
+          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+          Facebook</a>
+          <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+          Google+</a>
         </div>
         <!-- /.social-auth-links -->
         <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
       </div>
       <!-- /.login-box-body -->
     </div>
